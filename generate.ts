3084efccad9ca5242fafe01de4823e1323d62055
Arun Chandanaveli <aruncveli@gmail.com>
2024-06-08 22:05:01 +0200

    add trafikinformation tåg

--- generate.ts
+++ generate.ts
@@ -1,21 +1,36 @@
 import { argv } from "node:process";
-import { mkdirSync, writeFileSync } from "node:fs";
+import { mkdirSync, writeFileSync, appendFileSync } from "node:fs";
 import { renderFile } from "ejs";
 
 const name = argv[2];
 if (!name) {
   throw new Error("Name is required");
 }
-mkdirSync(name);
 
-renderFile("template.README.ejs", { name }, (err, str) => {
+const smallName = name.toLocaleLowerCase().replaceAll(" ", "-");
+console.log(`Creating directory ${smallName}`);
+mkdirSync(smallName);
+
+renderFile("readme.ejs", { name, smallName }, (err, str) => {
   if (err) throw err;
   console.log(`Writing README.md`);
-  writeFileSync(`${name}/README.md`, str);
+  writeFileSync(`${smallName}/README.md`, str);
 });
 
-renderFile("template.user.styl.ejs", { name }, (err, str) => {
+const date = new Date();
+const year = date.getFullYear().toString().substring(2);
+const month = date.getMonth().toString().padStart(2, "0");
+const day = date.getDate().toString().padStart(2, "0");
+const version = `${year}.${month}.${day}`;
+
+renderFile("user.styl.ejs", { name, smallName, version }, (err, str) => {
   if (err) throw err;
-  console.log(`Writing ${name}.user.styl`);
-  writeFileSync(`${name}/${name}.user.styl`, str);
+  console.log(`Writing ${smallName}.user.styl`);
+  writeFileSync(`${smallName}/${smallName}.user.styl`, str);
+});
+
+renderFile("root-readme-entry.ejs", { name, smallName }, (err, str) => {
+  if (err) throw err;
+  console.log(`Writing to README.md`);
+  appendFileSync(`README.md`, str);
 });
---
+++ trafikinformation-tåg/README.md
@@ -0,0 +1,9 @@
+# Trafikinformation tåg
+
+[![Install directly with Stylus](https://img.shields.io/badge/Install%20directly%20with-Stylus-238b8b.svg)](https://github.com/aruncveli/userstyles/raw/main/trafikinformation-tåg/trafikinformation-tåg.user.styl)
+
+Official [train info](https://www.trafikverket.se/trafikinformation/tag) page from Trafikverket.
+
+Link and link hover color variables are accepting inputs as `R,G,B` string, since Boostrap `rgb` variables need them so.
+
+![Screenshot of page](screenshot.png)
