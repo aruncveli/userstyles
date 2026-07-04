5ff7864d61274cf97e3dd9affa1bad961bc7a9e7
Arun Chandanaveli <aruncveli@gmail.com>
2026-07-04 11:15:45 +0200

    format

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,7 +1,4 @@
-import {
-  appendFileSync,
-  mkdirSync,
-} from "node:fs";
+import { appendFileSync, mkdirSync } from "node:fs";
 import { argv } from "node:process";
 
 import { compile } from "handlebars";
@@ -22,23 +19,19 @@ const smallName = args.join("-").toLocaleLowerCase();
 console.log(`Creating directory sites/${smallName}`);
 mkdirSync(`sites/${smallName}`);
 
-const read = (name: string) =>
-  Bun.file(`${import.meta.dir}/${name}`).text();
+const read = (name: string) => Bun.file(`${import.meta.dir}/${name}`).text();
 
 const [readme, userCss, rootEntry] = await Promise.all([
   read("readme.hbs"),
   read("user.css.hbs"),
   read("root-readme-entry.hbs"),
-]).then(files => files.map(file => compile(file)));
+]).then((files) => files.map((file) => compile(file)));
 
 console.log(`Writing sites/${smallName}/README.md`);
 await Bun.write(`sites/${smallName}/README.md`, readme!({ name, smallName }));
 
 console.log(`Writing sites/${smallName}/${smallName}.user.css`);
-await Bun.write(
-  `sites/${smallName}/${smallName}.user.css`,
-  userCss!({ name, smallName, version }),
-);
+await Bun.write(`sites/${smallName}/${smallName}.user.css`, userCss!({ name, smallName, version }));
 
 console.log("Adding entry to root README.md table");
 appendFileSync("README.md", rootEntry!({ name, smallName }));
