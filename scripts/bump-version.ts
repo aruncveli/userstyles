c723b8d24ed069515f734d3979ed0e3925edd499
Arun Chandanaveli <aruncveli@gmail.com>
2026-07-21 22:25:53 +0200

    change @-moz-document to @document
    use sd instead of sed for bump version

--- scripts/bump-version.ts
+++ scripts/bump-version.ts
@@ -5,6 +5,6 @@ import version from "./calver.js";
 process.argv.slice(2).forEach(async (arg) => {
   const versionLineBlame = await $`git blame -L4,4 ${arg}`.text();
   if (!versionLineBlame.includes(version)) {
-    await $`sed --in-place '4s/.*/@version ${version}/' ${arg}`;
+    await $`sd '@version .*' '@version ${version}' ${arg}`;
   }
 });
